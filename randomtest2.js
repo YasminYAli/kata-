9df40c90898b2512b63342a1f9877801e3c55ccb
YasminYAli <yasminyusuf9@gmail.com>
2020-02-18 09:31:22 +0000

    remove characters

---
+++ randomtest2.js
@@ -0,0 +1,8 @@
+// Remove characters
+// You’re checking over a pile of application forms containing confidential data. You want to redact sensitive information first before you forward these in an email by removing first and last letters of a name (string). Write a function that removes the first and last letters of a name(string). Assume name is not less than 2 letters.
+
+function removeLetter() {}
+
+module.exports = {
+  removeLetter
+};
